9d5888dfcc6f246fc83dfcfba181f5abaea1b48f
EC2 Default User <ec2-user@ip-172-31-25-169.eu-west-2.compute.internal>
2020-07-21 19:30:27 +0000

    Fifth

--- scrabble/lib/scrabble.rb
+++ scrabble/lib/scrabble.rb
@@ -2,21 +2,29 @@ class Scrabble
   def initialize(word)
     @word = word 
   end
-  one = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
+  def check_for_one(letter)
+    one = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
+    if one.include? (letter.upcase)
+      return 1
+    else 
+      return 0
+    end
+  end
   def score
     if @word == nil
       return 0
     elsif @word.rstrip == ''
       return 0
-    elsif @word == 'a'
-      return 1
-    elsif @word == 'f'
-      return 4
+#    elsif @word == 'a'
+#     return 1
+#    elsif @word == 'f'
+#      return 4
+    else 
+      return check_for_one(@word)
     end
   end
 end
 
-puts Scrabble.new('f').score
+puts Scrabble.new('a').score
 
 
-A, E, I, O, U, L, N, R, S, T
